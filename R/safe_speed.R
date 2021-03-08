b78a90fa89cec32ebc1888dcd707be4ef56e0e63
Ruoyu-Wang108 <865524384@qq.com>
2021-03-08 00:39:01 -0800

    added some description

--- R/safe_speed.R
+++ R/safe_speed.R
@@ -1,31 +1,33 @@
-# Ruoyu Wang
-# Assignment 3 part 1
-# Using "while" loop to create a function
+#' Safe Speed
+#' @author Ruoyu Wang
+#'
+#' @description Estimate the highest "velocity" to stop a car for a given distance for different road (with different friction coefficient)
+#'
+#' @param v The starting speed for calculation. After each attempt, the velocity will increase 0.1 unit. (m/s)
+#' @param safe_distance The goal of braking distance. (m)
+#' @param mu The friction coefficient, need to identify for different roads.
+#' @param g The acceleration of gravity, a constant. (9.8 m/s)
+#' @param d The technical braking distance to be calculate in the loop, the initial default is 0. (m)
+#'
+#' @return The highest speed estimate until the target braking distance is reached. Numeric.
 
-# Estimate the highest "velocity" to stop a car for a given distance for different road (with different friction coefficient)
-
-# Variables
-# v, in m/s, the starting speed for calculation. After each attempt, the velocity will increase 0.1 unit
-# safe_distance, in m, the goal of braking distance
-# mu, no unit, the friction coefficient, need to identify for different roads.
-# g, 9.8, in m/s2, acceleration of gravity, a constant.
-# d, in m, the technical braking distance to be calculate in the loop, the initial default is 0
 
 
-safe_speed <- function(v, mu, safe_distance, 
+safe_speed <- function(v, mu, safe_distance,
                        g = 9.8, d = 0){
-  
-  v = if(v < 0) {stop("Speed cannot be less than zero!")} else {v}
-          
-  
-  safe_distance = ifelse(safe_distance < 0, 
-                         stop("Target Distance cannot be less than zero!"), 
+
+  v = if(v < 0) {stop("Speed cannot be less than zero!")}
+  else {v}
+
+
+  safe_distance = ifelse(safe_distance < 0,
+                         stop("Target Distance cannot be less than zero!"),
                          safe_distance)
-  
+
   i = 1 # time tracker
-  
+
   while ((d < safe_distance) && (i < 1000))  {
-    d = v^2 / (2 * mu * g) 
+    d = v^2 / (2 * mu * g)
     v = v + 0.1
     i = i + 1
   }
