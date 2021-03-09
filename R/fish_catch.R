f23eb687d1f49d820c8d6badc9c2ad7806f7bcb4
quin-smith <quinlan_smith@yahoo.com>
2021-03-09 11:15:23 -0800

    trying to get documentation to work

---
+++ R/fish_catch.R
@@ -0,0 +1,14 @@
+#' fish_catch
+#'
+#' @description Contains records of 300 fish caught
+#' @format A data frame with 300 rows and 1 variable
+#' @source Dr. Naomi Tague & Will Burke
+#' \itemize{
+#'   \item fish. type of fish caught
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name fish_catch
+#' @usage data(fish_catch)
+"data/fish_catch.rda"
