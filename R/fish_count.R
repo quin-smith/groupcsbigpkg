b5290301b8804172d608c27fe9161a95f2f1c795
Ruoyu-Wang108 <865524384@qq.com>
2021-03-10 10:27:32 -0800

    fixed an error in function: fish_count

--- R/fish_count.R
+++ R/fish_count.R
@@ -10,21 +10,21 @@
 #' @examples
 fish_count <- function(fish, hist.plot = FALSE){
   # Add error if input is not a character/factor:
-  if((class(fish) == "character") == FALSE){
-    return("fish is not a character vector!")
-  }
-  fish = as.factor(fish)
-  # Prepare outputs
-  most_fish = names(which.max(summary(fish)))
-  rarest_fish = names(which.min(summary(fish)))
-  total_fish = sum(summary(fish))
+  if((class(fish) == "character")){
+    fish = as.factor(fish)
+    # Prepare outputs
+    most_fish = names(which.max(summary(fish)))
+    rarest_fish = names(which.min(summary(fish)))
+    total_fish = sum(summary(fish))
 
-  # Conditional histogram:
-  if(hist.plot) {
-    return(list(most = most_fish, rarest = rarest_fish, total = total_fish, fish_plot = plot(fish,
-                                                                                           main = sprintf("%i Total Fish Caught", total_fish))))
-  } else {
-    return(list(most = most_fish, rarest = rarest_fish, total = total_fish))
-    }
+    # Conditional histogram:
+    if(hist.plot) {
+      return(list(most = most_fish, rarest = rarest_fish, total = total_fish, fish_plot = plot(fish,
+                                                                                               main = sprintf("%i Total Fish Caught", total_fish))))
+    } else {
+      return(list(most = most_fish, rarest = rarest_fish, total = total_fish))
+  }
+
+    return("fish is not a character vector!")}
 
 }
