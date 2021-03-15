099cbbfb5492228638460dd45f8072ce9e26edab
quin-smith <quinlan_smith@yahoo.com>
2021-03-15 15:04:28 -0700

    added more tests... i think we're done

--- tests/testthat/testld_50.R
+++ tests/testthat/testld_50.R
@@ -3,3 +3,12 @@ context("ld_50() test")
 test_that("170 lbs returns 122 cups", {
   expect_output(ld_50(170), cat(c(122, "cups will kill you")))
 })
+
+test_that("Error checking message works", {
+  expect_match(ld_50(0), "Please enter a positive number for weight")
+  expect_match(ld_50(-1), "Please enter a positive number for weight")
+})
+
+test_that("1 lbs returns 1 cup", {
+  expect_match(ld_50(1), "1 cup will kill you")
+})
